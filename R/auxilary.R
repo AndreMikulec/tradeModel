7530ec23bcce76cc4867ff35a9756d125b39ac71
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2019-03-02 22:32:32 -0600

    fst garbage collect

--- R/auxilary.R
+++ R/auxilary.R
@@ -550,6 +550,7 @@ initEnv();on.exit({uninitEnv()})
         # remove dead columns (I choose to do that here)
         if(!is.null(ColRmExpr)) {
           Dbf <- Dbf[, stringr::str_subset(colnames(Dbf), stringr::str_c(ColRmExpr, collapse = "|"), negate = TRUE),drop = F]
+          gc()
         }
         # write an FST file
         if(getOption("tradeModel__makeFSTFiles__verbose") == TRUE)
